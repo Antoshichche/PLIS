10679b2d613e41d4e9b4229921e5e341da4bb773
Antoshichche <119878819+Antoshichche@users.noreply.github.com>
2026-05-16 18:07:37 +0300

    Add files via upload

---
+++ Lab5.srcs/sources_1/ip/fifo_generator_0/fifo_generator_0_stub.v
@@ -0,0 +1,31 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.2 (win64) Build 1909853 Thu Jun 15 18:39:09 MDT 2017
+// Date        : Mon Apr 20 18:47:46 2026
+// Host        : DESKTOP-ILITBPN running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/joker/Lab5_Project/Lab5.srcs/sources_1/ip/fifo_generator_0/fifo_generator_0_stub.v
+// Design      : fifo_generator_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "fifo_generator_v13_1_4,Vivado 2017.2" *)
+module fifo_generator_0(rst, wr_clk, rd_clk, din, wr_en, rd_en, dout, full, 
+  empty, prog_full, prog_empty)
+/* synthesis syn_black_box black_box_pad_pin="rst,wr_clk,rd_clk,din[15:0],wr_en,rd_en,dout[15:0],full,empty,prog_full,prog_empty" */;
+  input rst;
+  input wr_clk;
+  input rd_clk;
+  input [15:0]din;
+  input wr_en;
+  input rd_en;
+  output [15:0]dout;
+  output full;
+  output empty;
+  output prog_full;
+  output prog_empty;
+endmodule
